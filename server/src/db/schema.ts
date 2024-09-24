03db2bc79a182f9877dd7daba7c86d70ec882fb2
Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2024-09-24 05:44:55 -0300

    ajuste do tipo de dados do banco

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,5 +1,5 @@
-import { pgTable, text, timestamp, integer } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const goals = pgTable('goals', {
   id: text('id')
